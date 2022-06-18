474ae681943f24d74edf2779bb12b0e68607bfe1
TUNCAY <79506815+tekmez@users.noreply.github.com>
2022-06-18 17:53:40 +0300

    Create sqlLike.sql

---
+++ sqlLike.sql
@@ -0,0 +1,8 @@
+-- 1-) 
+SELECT * FROM country WHERE country LIKE 'A%a';
+-- 2-) 
+SELECT * FROM country WHERE country LIKE '_____a';
+-- 3-) 
+SELECT * FROM film WHERE title ILIKE '%t%t%t%t%';
+-- 4-) 
+SELECT * FROM film WHERE title LIKE 'C%' AND length > 90 AND rental_rate=2.99;
